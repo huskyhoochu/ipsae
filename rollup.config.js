85bb18ea19c9c709cab3d7b627c0744912c8c5a3
huskyhoochu <dfg1499@gmail.com>
2022-07-23 19:01:21 +0900

    setup build flow

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { swc, minify } from 'rollup-plugin-swc3';
+
+export default opts => {
+  const options = Object.assign({}, opts);
+
+  /**
+   * @type {import('rollup').RollupOptions}
+   */
+  const config = {
+    input: options.input,
+    output: [
+      { format: 'cjs', file: './dist/index.cjs.js', sourcemap: false },
+      {
+        format: 'umd',
+        file: './dist/index.min.js',
+        sourcemap: false,
+        plugins: [minify()],
+        name: options.name,
+      },
+    ],
+    plugins: [swc()],
+  };
+
+  return config;
+};
